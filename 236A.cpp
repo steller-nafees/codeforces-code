54611649ca0b86a1cb4afef20eaf2a0fa5c42919
Nafees N Islam <151987640+steller-nafees@users.noreply.github.com>
2025-05-15 20:11:08 +0600

    Update 236A.cpp

--- 236A.cpp
+++ 236A.cpp
@@ -1,43 +1,63 @@
+//============================================================================
+// 236A. Boy or Girl - Codeforces
+// Author: H M Nafees N Islam
+// Institution: North South University (NSU), CSE
+// Description: Checks the number of distinct characters to determine the output
+//============================================================================
+
+/*
+  بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ
+  Start with the name of Allah
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+// Fast I/O
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+// Type aliases
+using ll = long long;
+using ull = unsigned long long;
+
+// Debug (disabled in submission)
+#ifdef LOCAL
+    #define debug(x) cerr << #x << " = " << x << '\n';
+#else
+    #define debug(x)
+#endif
+
+int main() {
+    FAST_IO;
+
     string s;
-    cin>>s;
+    cin >> s;
 
-    int let[26] = {0};
-    int a;
-
-    for(int i=0 ; i<s.length(); i++)
-    {
-        a = s[i] - 'a';
-        let[a]++;
+    int freq[26] = {0};
+    for (char ch : s) {
+        freq[ch - 'a']++;
     }
 
-    int letter=0;
-    for(int i=0; i<26; i++)
-    {
-        if(let[i] != 0)
-        {
-            letter++;
-        }
+    int distinct = 0;
+    for (int i = 0; i < 26; i++) {
+        if (freq[i]) distinct++;
     }
 
-    if(letter%2 == 0)
-    {
-        cout<<"CHAT WITH HER!"<<endl;
-    }
+    if (distinct % 2 == 0)
+        cout << "CHAT WITH HER!" << '\n';
     else
-    {
-        cout<<"IGNORE HIM!"<<endl;
-    }
-
-
+        cout << "IGNORE HIM!" << '\n';
 
     return 0;
 }
 
+/*
+  ## 💡 Solution Logic
+  Count the number of distinct letters in the input string.
+  If it's even, print "CHAT WITH HER!", else print "IGNORE HIM!".
+*/
 
-
+/*
+  Alhamdulillah, problem solved!
+  الحمد لله الذي بنعمته تتم الصالحات
+*/
