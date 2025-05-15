0c3807e13bc04a28ce7379ad1807d65f023d9d6a
Nafees N Islam <151987640+steller-nafees@users.noreply.github.com>
2025-05-15 20:17:08 +0600

    Update 339A.cpp

--- 339A.cpp
+++ 339A.cpp
@@ -1,56 +1,73 @@
-//A. Helpful Maths - Codeforce
+//============================================================================
+// 339A. Helpful Maths - Codeforces
+// Author: H M Nafees N Islam
+// Institution: North South University (NSU), CSE
+// Description: Sort numbers separated by '+' and print them in ascending order
+//============================================================================
+
+/*
+  بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ
+  Start with the name of Allah
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Fast I/O
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-int main()
-{
+// Type aliases
+using ll = long long;
+using ull = unsigned long long;
+
+// Debug (disabled in submission)
+#ifdef LOCAL
+    #define debug(x) cerr << #x << " = " << x << '\n';
+#else
+    #define debug(x)
+#endif
+
+int main() {
+    FAST_IO;
+
     string s;
-    cin>>s;
+    cin >> s;
 
-    int one =0, two = 0, three = 0;
-    int index = 0;
-    for(int i=0; i<s.length(); i++)
-    {
-        char a = s[i];
-        if(a == '1')
-            one++;
-        else if(a == '2')
-            two++;
-        else if(a == '3')
-            three++;
+    int one = 0, two = 0, three = 0;
+
+    for (char ch : s) {
+        if (ch == '1') one++;
+        else if (ch == '2') two++;
+        else if (ch == '3') three++;
     }
 
-    int x = one+two+three;
+    int total = one + two + three;
 
-    for(int i= 0; i<x; i++)
-    {
-        if(one != 0)
-        {
-            cout<<"1";
+    for (int i = 0; i < total; i++) {
+        if (one > 0) {
+            cout << "1";
             one--;
-        }
-        else if(two != 0)
-        {
-            cout<<"2";
+        } else if (two > 0) {
+            cout << "2";
             two--;
-        }
-        else if(three != 0)
-        {
-            cout<<"3";
+        } else {
+            cout << "3";
             three--;
         }
-        if(i != x - 1)
-            cout<<"+";
+        if (i != total - 1) cout << "+";
     }
+    cout << '\n';
+
     return 0;
 }
 
+/*
+  ## 💡 Solution Logic
+  Count how many times '1', '2', and '3' appear.
+  Then print them in ascending order separated by '+'.
+*/
 
 /*
-
-## 💡 Solution Logic
-
-take 3 inputs and sum should be equal or greater that 2 for increment in solved
+  Alhamdulillah, problem solved!
+  الحمد لله الذي بنعمته تتم الصالحات
 */
