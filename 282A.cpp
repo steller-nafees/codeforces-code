83ae7022faa46b6e529bde7023ce3a7729b25f97
Nafees N Islam <151987640+steller-nafees@users.noreply.github.com>
2025-05-15 20:16:31 +0600

    Update 282A.cpp

--- 282A.cpp
+++ 282A.cpp
@@ -1,57 +1,64 @@
-//A. Bits++ - Codeforce
+//============================================================================
+// 282A. Bits++ - Codeforces
+// Author: H M Nafees N Islam
+// Institution: North South University (NSU), CSE
+// Description: Simulates increment/decrement operations based on input strings
+//============================================================================
+
+/*
+  بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ
+  Start with the name of Allah
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+// Fast I/O
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+// Type aliases
+using ll = long long;
+using ull = unsigned long long;
+
+// Debug (disabled in submission)
+#ifdef LOCAL
+    #define debug(x) cerr << #x << " = " << x << '\n';
+#else
+    #define debug(x)
+#endif
+
+int main() {
+    FAST_IO;
+
     int n;
     cin >> n;
 
-    int x=0;
-
-    while (n--)
-    {
+    int x = 0;
+    while (n--) {
         string str;
-        cin>>str;
+        cin >> str;
 
-        //Condition for ++x
-        if(str[0]=='+' && str[1] == '+')
-        {
-            ++x;
-        }
-        //Condition for x++
-        else if(str[1]=='+' && str[2] == '+')
-        {
+        if (str[1] == '+') {
             x++;
-        }
-        //Condition for --x
-        else if(str[0]=='-' && str[1] == '-')
-        {
-            --x;
-        }
-        //Condition for x--
-        else if(str[1]=='-' && str[2] == '-')
-        {
+        } else if (str[1] == '-') {
             x--;
         }
-        else
-        {
-            continue;
-        }
-
-
     }
-    cout<<x<<endl;
 
-
+    cout << x << '\n';
     return 0;
 }
 
+/*
+  ## 💡 Solution Logic
+  Input consists of statements like "++X", "X++", "--X", "X--".
+  Only the middle character matters:
+    - If it's '+', increment `x`
+    - If it's '-', decrement `x`
+  Final value of `x` is the answer.
+*/
 
 /*
-
-## 💡 Solution Logic
-
-take 3 inputs and sum should be equal or greater that 2 for increment in solved
+  Alhamdulillah, problem solved!
+  الحمد لله الذي بنعمته تتم الصالحات
 */
