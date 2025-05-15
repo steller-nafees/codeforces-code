5b7d9b8595ad9a72f25840be4a500bffc13edeb8
Nafees N Islam <151987640+steller-nafees@users.noreply.github.com>
2025-05-15 20:09:58 +0600

    Update 231A.cpp

--- 231A.cpp
+++ 231A.cpp
@@ -1,35 +1,59 @@
-//A. Teams - Codeforce
+//============================================================================
+// 231A. Teams - Codeforces
+// Author: H M Nafees N Islam
+// Institution: North South University (NSU), CSE
+// Description: Count how many problems were solved by the team
+//============================================================================
+
+/*
+  بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ
+  Start with the name of Allah
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+// Fast I/O
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+// Type aliases
+using ll = long long;
+using ull = unsigned long long;
+
+// Debug (disabled in submission)
+#ifdef LOCAL
+    #define debug(x) cerr << #x << " = " << x << '\n';
+#else
+    #define debug(x)
+#endif
+
+int main() {
+    FAST_IO;
+
     int n;
     cin >> n;
-    int solved=0;
+    int solved = 0;
 
-    while (n--)
-    {
-        int a,b,c;
-        a=b=c=0;
-        cin>>a>>b>>c;
-        if(a+b+c >= 2)
-        {
+    while (n--) {
+        int a = 0, b = 0, c = 0;
+        cin >> a >> b >> c;
+        if (a + b + c >= 2) {
             solved++;
         }
     }
 
-    cout<<solved<<endl;
-
-
+    cout << solved << '\n';
     return 0;
 }
 
+/*
+  ## 💡 Solution Logic
+  Take 3 inputs representing each team member's confidence (1 or 0).
+  If the sum is 2 or more, the team is confident enough to solve the problem.
+  Increment `solved` for each such case.
+*/
 
 /*
-
-## 💡 Solution Logic
-
-take 3 inputs and sum should be equal or greater that 2 for increment in solved
+  Alhamdulillah, problem solved!
+  الحمد لله الذي بنعمته تتم الصالحات
 */
