fd0660c99e5f79e2095cce25a2fe6f902e1e7505
steller-nafees <nafeesnislam.official@gmail.com>
2025-11-14 23:58:17 +0600

    Update code

---
+++ Practice/Catagory A/1814A. Coins/code/1814A.cpp
@@ -0,0 +1,142 @@
+//============================================================================
+// Platform      : Codeforces 
+// Problem Title : 1814A. Coins
+// Author        : H M Nafees N Islam
+// Institution   : North South University (NSU), Computer Science And Engineering
+// Date          : November 14, 2025 
+//
+// Problem Statement:
+//   In Berland, there are two types of coins with denominations 2 and k burles.
+//   You must determine whether it is possible to form exactly n burles using
+//   any non-negative number of these coins.
+//
+//   Formally, determine if there exist non-negative integers x and y such that:
+//         2⋅x + k⋅y = n
+//
+// Input:
+//   The first line contains an integer t (1 ≤ t ≤ 10^4) — number of test cases.
+//   Each test case consists of two integers n and k (1 ≤ k ≤ n ≤ 10^18, k ≠ 2).
+//
+// Output:
+//   For each test case, print "YES" if n can be represented using coins of
+//   denominations 2 and k. Otherwise, print "NO".
+//
+// Constraints:
+//   - 1 ≤ t ≤ 10^4
+//   - 1 ≤ k ≤ n ≤ 10^18
+//   - k ≠ 2
+//   - Time limit: 2 seconds
+//   - Memory limit: 256 MB
+//============================================================================
+
+/*
+  بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ
+  Start with the name of Allah
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fast I/O
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define OUTPUT_CONDITION(x) x?"YES" : "NO"
+#define INPUT_ARRAY(a,n) for(int i = 0; i<n; i++) cin>>a[i];
+#define OUTPUT_2Values(o1,o2) cout<<o1<<" "<<o2<<endl
+#define SORT_DESC(a) sort(a.begin(), a.end(),greater<int>())
+#define SORT_ASC(a) sort(a.begin(), a.end())
+#define vi(a,n) vector<int> a(n);
+#define vl(a,n) vector<ll> a(n);
+#define output(a) cout<< a <<endl;
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define OUTPUT_ARRAY(a,n) for (int i = 0; i < n; i++) cout << a[i] << endl;
+#define vii(a,n,val) vector<int> a(n,val);
+#define INPUT_int(n) int n; cin>>n;
+#define INPUT_ll(n) ll n; cin>>n;
+#define INPUT_string(s) string s; cin>>s;
+#define INPUT_char(c) char c; cin>>c;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define STR_LEN(s) ((int)s.size())
+#define STR_REV(s) reverse(all(s))
+#define STR_SORT_ASC(s) sort(all(s))
+#define STR_SORT_DESC(s) sort(rall(s))
+#define STR_TOLOWER(s) transform(all(s), s.begin(), ::tolower)
+#define STR_TOUPPER(s) transform(all(s), s.begin(), ::toupper)
+#define STR_CONTAINS(s, sub) (s.find(sub) != string::npos)
+#define STR_SUBSTR(s, l, r) (s.substr(l, r))
+#define STR_APPEND(s1, s2) s1 += s2
+#define STR_STARTS_WITH(s, pref) (s.rfind(pref, 0) == 0)
+#define STR_ENDS_WITH(s, suff) (s.size() >= suff.size() && s.compare(s.size() - suff.size(), suff.size(), suff) == 0)
+#define STR_SPLIT(vec, s, delim) { string tmp; stringstream ss(s); while (getline(ss, tmp, delim)) vec.pb(tmp); }
+#define STR_JOIN(res, vec, delim) { res = ""; for (int i = 0; i < (int)vec.size(); i++) { res += vec[i]; if (i + 1 != (int)vec.size()) res += delim; } }
+#define STR_TO_INT(s) stoi(s)
+#define STR_TO_LL(s) stoll(s)
+#define INT_TO_STR(x) to_string(x)
+#define MIN_HEAP(pq) priority_queue<int, vector<int>, greater<int>>pq;
+#define MAX_HEAP(pq) priority_queue<int>pq;
+#define PQ_INPUT(pq) for(int i = 0; i<n; i++){INPUT_int(x);pq.push(x);}
+#define PQ_SUM(pq,sum) ll sum = 0;while(!pq.empty()){sum += pq.top();pq.pop();}
+#define Output_precision(pr,res) cout << fixed << setprecision(pr) << res << endl;
+// Type aliases
+using ll = long long;
+using ull = unsigned long long;
+
+// Debug (disabled in submission)
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << '\n';
+#else
+#define debug(x)
+#endif
+
+void solve()
+{
+    INPUT_ll(n);
+    INPUT_ll(k);
+
+    if(n%2 == 0) 
+    {
+        YES
+    }
+    else 
+    {
+        if(k%2 == 1 && n >= k)
+        {
+            YES
+        }
+        else
+        {
+            NO
+        }
+    }
+}
+
+int main()
+{
+    FAST_IO;
+
+    int t; cin>>t; 
+    while (t--) solve();
+    
+    //For single test case, comment the above 3 lines and uncomment below line
+    //solve();
+
+    return 0;
+}
+
+/*
+   Solution Logic:
+   - We want to check if n can be expressed as: n = 2·x + k·y.
+   - If n is even, it can always be formed using only 2-burle coins, so answer is YES.
+   - If n is odd, we must use at least one k-coin.
+   - For n to be odd:
+       • k must also be odd (otherwise k·y is even, making RHS even).
+       • n must be ≥ k, so we can subtract one k-coin and the remainder becomes even.
+   - These checks match the derived number-theory conditions required
+     to form n using coins of denominations 2 and k.
+*/
+
+/*
+  Alhamdulillah, problem solved successfully!
+  الحمد لله الذي بنعمته تتم الصالحات
+*/
