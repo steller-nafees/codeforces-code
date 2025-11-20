21df76ce7bc31052296bb96c4c827e44b1188d93
steller-nafees <nafeesnislam.official@gmail.com>
2025-11-20 20:24:51 +0600

    Update code

---
+++ Practice/Catagory C/903C. Boxes Packing/code/903C.cpp
@@ -0,0 +1,136 @@
+//============================================================================
+// Platform      : Codeforces 
+// Problem Title : 903C. Boxes Packing
+// Author        : H M Nafees N Islam
+// Institution   : North South University (NSU), Computer Science And Engineering
+// Date          : November 20, 2025 
+//
+// Problem Statement:
+//   Mishka has n boxes, each being a cube with side length ai.
+//   He can place box i into box j if all conditions hold:
+//     - box i is not already inside another box
+//     - box j contains no other box
+//     - ai < aj
+//
+//   Boxes can be nested multiple times. A box is visible if it is not 
+//   placed into another box. Mishka wants to minimize the number of 
+//   visible boxes.
+//
+// Input:
+//   The first line contains n (1 ≤ n ≤ 5000).
+//   The second line contains n integers a1, a2, ..., an (1 ≤ ai ≤ 1e9).
+//
+// Output:
+//   Print the minimum number of visible boxes.
+//
+// Constraints:
+//  - Time limit: 1 second
+//  - Memory limit: 256 MB
+//============================================================================
+
+/*
+  بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ
+  Start with the name of Allah
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fast I/O
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define OUTPUT_CONDITION(x) x?"YES" : "NO"
+#define INPUT_ARRAY(a,n) for(int i = 0; i<n; i++) cin>>a[i];
+#define OUTPUT_2Values(o1,o2) cout<<o1<<" "<<o2<<endl
+#define SORT_DESC(a) sort(a.begin(), a.end(),greater<int>())
+#define SORT_ASC(a) sort(a.begin(), a.end())
+#define vi(a,n) vector<int> a(n);
+#define vl(a,n) vector<ll> a(n);
+#define output(a) cout<< a <<endl;
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define OUTPUT_ARRAY(a,n) for (int i = 0; i < n; i++) cout << a[i] << endl;
+#define vii(a,n,val) vector<int> a(n,val);
+#define INPUT_int(n) int n; cin>>n;
+#define INPUT_ll(n) ll n; cin>>n;
+#define INPUT_string(s) string s; cin>>s;
+#define INPUT_char(c) char c; cin>>c;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define STR_LEN(s) ((int)s.size())
+#define STR_REV(s) reverse(all(s))
+#define STR_SORT_ASC(s) sort(all(s))
+#define STR_SORT_DESC(s) sort(rall(s))
+#define STR_TOLOWER(s) transform(all(s), s.begin(), ::tolower)
+#define STR_TOUPPER(s) transform(all(s), s.begin(), ::toupper)
+#define STR_CONTAINS(s, sub) (s.find(sub) != string::npos)
+#define STR_SUBSTR(s, l, r) (s.substr(l, r))
+#define STR_APPEND(s1, s2) s1 += s2
+#define STR_STARTS_WITH(s, pref) (s.rfind(pref, 0) == 0)
+#define STR_ENDS_WITH(s, suff) (s.size() >= suff.size() && s.compare(s.size() - suff.size(), suff.size(), suff) == 0)
+#define STR_SPLIT(vec, s, delim) { string tmp; stringstream ss(s); while (getline(ss, tmp, delim)) vec.pb(tmp); }
+#define STR_JOIN(res, vec, delim) { res = ""; for (int i = 0; i < (int)vec.size(); i++) { res += vec[i]; if (i + 1 != (int)vec.size()) res += delim; } }
+#define STR_TO_INT(s) stoi(s)
+#define STR_TO_LL(s) stoll(s)
+#define INT_TO_STR(x) to_string(x)
+#define MIN_HEAP(pq) priority_queue<int, vector<int>, greater<int>>pq;
+#define MAX_HEAP(pq) priority_queue<int>pq;
+#define PQ_INPUT(pq) for(int i = 0; i<n; i++){INPUT_int(x);pq.push(x);}
+#define PQ_SUM(pq,sum) ll sum = 0;while(!pq.empty()){sum += pq.top();pq.pop();}
+#define Output_precision(pr,res) cout << fixed << setprecision(pr) << res << endl;
+#define uo_map(a,keytype, valuetype) unordered_map<keytype,valuetype> a;
+#define umap_input(a,n,dtype) for(int i = 0; i<n; i++){dtype x; cin>>x; a[x]++;}
+#define umap_freqCount(a, freq) for(auto x : a){freq[x]++;}
+// Type aliases
+using ll = long long;
+using ull = unsigned long long;
+
+// Debug (disabled in submission)
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << '\n';
+#else
+#define debug(x)
+#endif
+
+void solve()
+{
+    INPUT_int(n);
+    ll a;
+    uo_map(freq,ll,int)
+    int max_freq = 0;
+    for(int i=0; i<n; i++)
+    {
+        cin >> a; 
+        freq[a]++;
+        max_freq = max(max_freq, freq[a]);
+    }
+    output(max_freq);
+
+}
+
+int main()
+{
+    FAST_IO;
+
+    //int t; cin>>t; 
+    //while (t--) solve();
+    
+    //For single test case, comment the above 3 lines and uncomment below line
+    solve();
+
+    return 0;
+}
+
+/*
+   Solution Logic:
+   - To minimize the number of visible boxes, Mishka should nest as many boxes as possible.
+   - A box can only be placed inside a strictly larger box that currently contains no other box.
+   - The limitation comes from how many boxes of the same size exist.
+   - Multiple boxes of the same side length cannot be nested into each other → each such group contributes at least `count_of_that_size` visible boxes.
+   - Therefore, the minimum number of visible boxes is equal to the maximum frequency of any side length.
+   - The code counts occurrences of each ai and outputs the largest frequency.
+*/
+
+/*
+  Alhamdulillah, problem solved successfully!
+  الحمد لله الذي بنعمته تتم الصالحات
+*/
