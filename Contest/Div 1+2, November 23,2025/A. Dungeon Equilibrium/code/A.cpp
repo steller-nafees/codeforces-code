cd2d339f9a4f0951d6498bfa90d4b3fa42be0912
steller-nafees <nafeesnislam.official@gmail.com>
2025-11-23 16:47:59 +0600

    Update code

---
+++ Contest/Div 1+2, November 23,2025/A. Dungeon Equilibrium/code/A.cpp
@@ -0,0 +1,150 @@
+//============================================================================
+// Platform      : Codeforces 
+// Problem Title : A. Dungeon Equilibrium
+// Author        : H M Nafees N Islam
+// Institution   : North South University (NSU), Computer Science And Engineering
+// Date          : November 23, 2025 
+//
+// Problem Statement:
+//   An array is called balanced if every integer x that occurs at least once
+//   occurs exactly x times in the array. You can delete some elements to
+//   make the array balanced. Determine the minimum number of deletions.
+//
+// Input:
+//   - The first line contains t — number of test cases (1 ≤ t ≤ 500)
+//   - For each test case:
+//       - First line: integer n (1 ≤ n ≤ 100) — size of the array
+//       - Second line: n integers a1,a2,…,an (0 ≤ ai ≤ n)
+//
+// Output:
+//   - For each test case, print a single integer — minimum number of deletions
+//
+// Constraints:
+//   - No constraints on sum of n over all test cases
+//============================================================================
+
+/*
+  بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ
+  Start with the name of Allah
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fast I/O
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define OUTPUT_CONDITION(x) x?"YES" : "NO"
+#define INPUT_ARRAY(a,n) for(int i = 0; i<n; i++) cin>>a[i];
+#define OUTPUT_2Values(o1,o2) cout<<o1<<" "<<o2<<endl
+#define SORT_DESC(a) sort(a.begin(), a.end(),greater<int>())
+#define SORT_ASC(a) sort(a.begin(), a.end())
+#define vi(a,n) vector<int> a(n);
+#define vl(a,n) vector<ll> a(n);
+#define output(a) cout<< a <<endl;
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define OUTPUT_ARRAY(a,n) for (int i = 0; i < n; i++) cout << a[i] << endl;
+#define vii(a,n,val) vector<int> a(n,val);
+#define INPUT_int(n) int n; cin>>n;
+#define INPUT_ll(n) ll n; cin>>n;
+#define INPUT_string(s) string s; cin>>s;
+#define INPUT_char(c) char c; cin>>c;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define STR_LEN(s) ((int)s.size())
+#define STR_REV(s) reverse(all(s))
+#define STR_SORT_ASC(s) sort(all(s))
+#define STR_SORT_DESC(s) sort(rall(s))
+#define STR_TOLOWER(s) transform(all(s), s.begin(), ::tolower)
+#define STR_TOUPPER(s) transform(all(s), s.begin(), ::toupper)
+#define STR_CONTAINS(s, sub) (s.find(sub) != string::npos)
+#define STR_SUBSTR(s, l, r) (s.substr(l, r))
+#define STR_APPEND(s1, s2) s1 += s2
+#define STR_STARTS_WITH(s, pref) (s.rfind(pref, 0) == 0)
+#define STR_ENDS_WITH(s, suff) (s.size() >= suff.size() && s.compare(s.size() - suff.size(), suff.size(), suff) == 0)
+#define STR_SPLIT(vec, s, delim) { string tmp; stringstream ss(s); while (getline(ss, tmp, delim)) vec.pb(tmp); }
+#define STR_JOIN(res, vec, delim) { res = ""; for (int i = 0; i < (int)vec.size(); i++) { res += vec[i]; if (i + 1 != (int)vec.size()) res += delim; } }
+#define STR_TO_INT(s) stoi(s)
+#define STR_TO_LL(s) stoll(s)
+#define INT_TO_STR(x) to_string(x)
+#define MIN_HEAP(pq) priority_queue<int, vector<int>, greater<int>>pq;
+#define MAX_HEAP(pq) priority_queue<int>pq;
+#define PQ_INPUT(pq) for(int i = 0; i<n; i++){INPUT_int(x);pq.push(x);}
+#define PQ_SUM(pq,sum) ll sum = 0;while(!pq.empty()){sum += pq.top();pq.pop();}
+#define Output_precision(pr,res) cout << fixed << setprecision(pr) << res << endl;
+#define uo_map(a,keytype, valuetype) unordered_map<keytype,valuetype> a;
+#define umap_input(a,n,dtype) for(int i = 0; i<n; i++){dtype x; cin>>x; a[x]++;}
+#define umap_freqCount(a, freq) for(auto x : a){freq[x]++;}
+#define SET(s,dType)set<dType>s
+#define INSERT(s, x) s.insert(x)
+#define EXISTS(s, x) (s.find(x) != s.end())
+#define SIZE(s)(int)s.size()
+#define CLEAR(s)s.clear()
+// Type aliases
+using ll = long long;
+using ull = unsigned long long;
+
+// Debug (disabled in submission)
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << '\n';
+#else
+#define debug(x)
+#endif
+
+void solve()
+{
+    INPUT_int(n);
+    vi(a,n); INPUT_ARRAY(a,n);
+    uo_map(freq,int,int)
+
+    umap_freqCount(a,freq);
+
+    int del = 0;
+    for(auto &p : freq )
+    {
+        int x = p.first;
+        int count = p.second;
+
+        if(x == 0)
+        {
+            del += count;// delete zeros 
+        }
+        else if(count >= x)
+        {
+            del += (count - x); // delete extra of x
+        }
+        else//count < x
+        {
+            del += count;
+        }
+    }
+    output(del);
+}
+
+int main()
+{
+    FAST_IO;
+
+    int t; cin>>t; 
+    while (t--) solve();
+    
+    //For single test case, comment the above 3 lines and uncomment below line
+    //solve();
+
+    return 0;
+}
+
+/*
+  💡 Solution Logic:
+  - Count frequency of each integer in the array
+  - For 0, delete all occurrences
+  - For x > 0:
+      - If count >= x, delete the extra occurrences
+      - If count < x, delete all occurrences (cannot satisfy balanced)
+  - Sum all deletions to get the answer
+*/
+
+/*
+  Alhamdulillah, problem solved successfully!
+  الحمد لله الذي بنعمته تتم الصالحات
+*/
