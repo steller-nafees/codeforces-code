a605d108d0868db587e459fd5a96e9293a947975
H M Nafees N Islam <nafeesnislam.official@gmail.com>
2026-01-04 14:44:02 +0600

    Update code

---
+++ Practice/Catagory A/1955A. Yogurt Sale/code/1955A.cpp
@@ -0,0 +1,142 @@
+//============================================================================
+// Platform      : Codeforces 
+// Problem Title : 1955A. Yogurt Sale
+// Author        : H M Nafees N Islam
+// Institution   : North South University (NSU), Computer Science And Engineering
+// Date          : January 04, 2026 
+//
+// Problem Statement:
+//   The price of one yogurt at the "Vosmiorochka" store is a burles, but there
+//   is a promotion where two yogurts can be bought for b burles.
+//   Maxim needs to buy exactly n yogurts. When buying two yogurts, he can
+//   choose either the regular price or the promotional price.
+//   The task is to determine the minimum amount of money needed to buy
+//   exactly n yogurts.
+//
+// Input:
+//   The first line contains a single integer t — the number of test cases.
+//   Each test case consists of three integers:
+//     n — the number of yogurts to buy,
+//     a — the price of one yogurt,
+//     b — the price of two yogurts on promotion.
+//
+// Output:
+//   For each test case, print the minimum cost required to buy n yogurts.
+//
+// Constraints:
+//   - 1 ≤ t ≤ 10^4
+//   - 1 ≤ n ≤ 100
+//   - 1 ≤ a, b ≤ 30
+//   - Time limit: 1 second
+//   - Memory limit: 256 MB
+//============================================================================
+
+/*
+  بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ
+  Start with the name of Allah
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fast I/O
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define OUTPUT_CONDITION(x) x?"YES" : "NO"
+#define INPUT_ARRAY(a,n) for(int i = 0; i<n; i++) cin>>a[i];
+#define OUTPUT_2Values(o1,o2) cout<<o1<<" "<<o2<<endl
+#define SORT_DESC(a) sort(a.begin(), a.end(),greater<int>())
+#define SORT_ASC(a) sort(a.begin(), a.end())
+#define vi(a,n) vector<int> a(n);
+#define vl(a,n) vector<ll> a(n);
+#define output(a) cout<< a <<endl;
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define OUTPUT_ARRAY(a,n) for (int i = 0; i < n; i++) cout << a[i] << endl;
+#define vii(a,n,val) vector<int> a(n,val);
+#define INPUT_int(n) int n; cin>>n;
+#define INPUT_ll(n) ll n; cin>>n;
+#define INPUT_string(s) string s; cin>>s;
+#define INPUT_char(c) char c; cin>>c;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define STR_LEN(s) ((int)s.size())
+#define STR_REV(s) reverse(all(s))
+#define STR_SORT_ASC(s) sort(all(s))
+#define STR_SORT_DESC(s) sort(rall(s))
+#define STR_TOLOWER(s) transform(all(s), s.begin(), ::tolower)
+#define STR_TOUPPER(s) transform(all(s), s.begin(), ::toupper)
+#define STR_CONTAINS(s, sub) (s.find(sub) != string::npos)
+#define STR_SUBSTR(s, l, r) (s.substr(l, r))
+#define STR_APPEND(s1, s2) s1 += s2
+#define STR_STARTS_WITH(s, pref) (s.rfind(pref, 0) == 0)
+#define STR_ENDS_WITH(s, suff) (s.size() >= suff.size() && s.compare(s.size() - suff.size(), suff.size(), suff) == 0)
+#define STR_SPLIT(vec, s, delim) { string tmp; stringstream ss(s); while (getline(ss, tmp, delim)) vec.pb(tmp); }
+#define STR_JOIN(res, vec, delim) { res = ""; for (int i = 0; i < (int)vec.size(); i++) { res += vec[i]; if (i + 1 != (int)vec.size()) res += delim; } }
+#define STR_TO_INT(s) stoi(s)
+#define STR_TO_LL(s) stoll(s)
+#define INT_TO_STR(x) to_string(x)
+#define MIN_HEAP(pq) priority_queue<int, vector<int>, greater<int>>pq;
+#define MAX_HEAP(pq) priority_queue<int>pq;
+#define PQ_INPUT(pq) for(int i = 0; i<n; i++){INPUT_int(x);pq.push(x);}
+#define PQ_SUM(pq,sum) ll sum = 0;while(!pq.empty()){sum += pq.top();pq.pop();}
+#define Output_precision(pr,res) cout << fixed << setprecision(pr) << res << endl;
+// Type aliases
+using ll = long long;
+using ull = unsigned long long;
+
+// Debug (disabled in submission)
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << '\n';
+#else
+#define debug(x)
+#endif
+
+void solve()
+{
+    INPUT_int(n);
+    INPUT_int(a);
+    INPUT_int(b);
+
+    ll single_price = n * a;
+    ll offer_price = (n/2) * b, mixed_price = (n/2) * b + (n%2) * a;
+
+    ll min_price = single_price;
+
+    if(n % 2 == 0)
+    {
+        min_price = min(min_price, offer_price);
+    }
+    else
+    {
+        min_price = min(min_price, mixed_price);
+    }
+
+    output(min_price);
+}
+
+int main()
+{
+    FAST_IO;
+
+    int t; cin>>t; 
+    while (t--) solve();
+    
+    //For single test case, comment the above 3 lines and uncomment below line
+    //solve();
+
+    return 0;
+}
+
+/*
+   Solution Logic:
+   - Calculate the total cost if all yogurts are bought individually.
+   - Calculate the cost using as many promotional pairs as possible.
+   - If n is odd, buy the remaining one yogurt at regular price.
+   - Compare all valid options and choose the minimum cost.
+   - Output the minimum cost for each test case.
+*/
+
+/*
+  Alhamdulillah, problem solved successfully!
+  الحمد لله الذي بنعمته تتم الصالحات
+*/
