699d0627eefbf780230a411dfac0bf49a780dbf6
H M Nafees N Islam <nafeesnislam.official@gmail.com>
2026-03-11 10:54:03 +0600

    Update code

---
+++ Practice/Catagory A/318A. Even Odds/code/318A.cpp
@@ -0,0 +1,205 @@
+//============================================================================
+// Platform      : Codeforces 
+// Problem Title : 318A. Even Odds
+// Author        : H M Nafees N Islam
+// Institution   : North South University (NSU), Computer Science And Engineering
+// Date          : March 11, 2026 
+//
+// Problem Statement:
+//   Being a nonconformist, Volodya is displeased with the current order of
+//   natural numbers (positive integers). He decides to rearrange them.
+//   Since there are infinitely many natural numbers, he starts with the
+//   first n numbers.
+//
+//   He writes them in a new order:
+//     1. First all odd numbers from 1 to n in ascending order.
+//     2. Then all even numbers from 1 to n in ascending order.
+//
+//   After forming this sequence, we need to determine which number
+//   will appear at position k.
+//
+// Input:
+//   A single line containing two integers n and k.
+//
+// Output:
+//   Print the number that appears at position k in the rearranged sequence.
+//
+// Constraints:
+//   - 1 ≤ k ≤ n ≤ 10^12
+//   - Time limit: 1 second
+//   - Memory limit: 256 MB
+//============================================================================
+
+/*
+  بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ
+  Start with the name of Allah
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fast I/O
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define OUTPUT_CONDITION(x) x?"YES" : "NO"
+#define INPUT_ARRAY(a,n) for(int i = 0; i<n; i++) cin>>a[i];
+#define OUTPUT_2Values(o1,o2) cout<<o1<<" "<<o2<<endl
+#define SORT_DESC(a) sort(a.begin(), a.end(),greater<int>())
+#define SORT_ASC(a) sort(a.begin(), a.end())
+#define vi(a,n) vector<int> a(n);
+#define vl(a,n) vector<ll> a(n);
+#define output(a) cout<< a <<endl;
+#define YES cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define OUTPUT_ARRAY(a,n) for (int i = 0; i < n; i++) cout << a[i] << endl;
+#define vii(a,n,val) vector<int> a(n,val);
+#define INPUT_int(n) int n; cin>>n;
+#define INPUT_ll(n) ll n; cin>>n;
+#define INPUT_string(s) string s; cin>>s;
+#define INPUT_char(c) char c; cin>>c;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define STR_LEN(s) ((int)s.size())
+#define STR_REV(s) reverse(all(s))
+#define STR_SORT_ASC(s) sort(all(s))
+#define STR_SORT_DESC(s) sort(rall(s))
+#define STR_TOLOWER(s) transform(all(s), s.begin(), ::tolower)
+#define STR_TOUPPER(s) transform(all(s), s.begin(), ::toupper)
+#define STR_CONTAINS(s, sub) (s.find(sub) != string::npos)
+#define STR_SUBSTR(s, l, r) (s.substr(l, r))
+#define STR_APPEND(s1, s2) s1 += s2
+#define STR_STARTS_WITH(s, pref) (s.rfind(pref, 0) == 0)
+#define STR_ENDS_WITH(s, suff) (s.size() >= suff.size() && s.compare(s.size() - suff.size(), suff.size(), suff) == 0)
+#define STR_SPLIT(vec, s, delim) { string tmp; stringstream ss(s); while (getline(ss, tmp, delim)) vec.pb(tmp); }
+#define STR_JOIN(res, vec, delim) { res = ""; for (int i = 0; i < (int)vec.size(); i++) { res += vec[i]; if (i + 1 != (int)vec.size()) res += delim; } }
+#define STR_TO_INT(s) stoi(s)
+#define STR_TO_LL(s) stoll(s)
+#define INT_TO_STR(x) to_string(x)
+#define MIN_HEAP(pq) priority_queue<int, vector<int>, greater<int>>pq;
+#define MAX_HEAP(pq) priority_queue<int>pq;
+#define PQ_INPUT(pq) for(int i = 0; i<n; i++){INPUT_int(x);pq.push(x);}
+#define PQ_SUM(pq,sum) ll sum = 0;while(!pq.empty()){sum += pq.top();pq.pop();}
+#define Output_precision(pr,res) cout << fixed << setprecision(pr) << res << endl;
+
+// create maps
+#define umap(name, keytype, valuetype) unordered_map<keytype, valuetype> name;
+#define omap(name, keytype, valuetype) map<keytype, valuetype> name;
+
+// input & frequency
+#define umap_input_freq(mp, n, dtype) \
+for(int i = 0; i < n; i++){ dtype x; cin >> x; mp[x]++; }
+
+// convert map to vector (for sorting)
+#define map_to_vec(mp, vec) \
+vector<pair<decltype(mp.begin()->first), decltype(mp.begin()->second)>> vec(mp.begin(), mp.end());
+
+// sort by key
+#define sort_key_asc(vec) sort(vec.begin(), vec.end());
+#define sort_key_desc(vec) sort(vec.rbegin(), vec.rend());
+
+// sort by value
+#define sort_val_asc(vec) \
+sort(vec.begin(), vec.end(), [](auto &a, auto &b){ return a.second < b.second; });
+
+#define sort_val_desc(vec) \
+sort(vec.begin(), vec.end(), [](auto &a, auto &b){ return a.second > b.second; });
+
+// print
+#define print_map(mp) \
+for(auto &x : mp) cout << x.first << " " << x.second << "\n";
+
+#define print_vec(vec) \
+for(auto &x : vec) cout << x.first << " " << x.second << "\n";
+
+// pair type
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+
+// input
+#define pair_input(p) cin >> p.first >> p.second
+#define pair_input_vec(v, n) \
+for(int i = 0; i < n; i++) cin >> v[i].first >> v[i].second
+
+// sort by first
+#define sort_pair_first_asc(v) sort(v.begin(), v.end())
+#define sort_pair_first_desc(v) sort(v.rbegin(), v.rend())
+
+// sort by second
+#define sort_pair_second_asc(v) \
+sort(v.begin(), v.end(), [](auto &a, auto &b){ return a.second < b.second; });
+
+#define sort_pair_second_desc(v) \
+sort(v.begin(), v.end(), [](auto &a, auto &b){ return a.second > b.second; });
+
+// print
+#define print_pair(p) cout << p.first << " " << p.second << "\n"
+#define print_pair_vec(v) \
+for(auto &x : v) cout << x.first << " " << x.second << "\n";
+
+/* ================================================ */
+// Type aliases
+using ll = long long;
+using ull = unsigned long long;
+
+
+// Debug (disabled in submission)
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << '\n';
+#else
+#define debug(x)
+#endif
+
+void solve()
+{
+    int64_t n, k ; cin>> n >> k;
+    
+    int64_t ans = 0;
+    if(k <= (n + 1) / 2)
+    {
+        ans = 2 * k - 1;//odd
+    }
+    else
+    {
+        ans = 2 * (k - (n + 1) / 2); // even
+    }
+    output(ans);
+    
+}
+
+int main()
+{
+    FAST_IO;
+
+    //int t; cin>>t; 
+    //while (t--) solve();
+    
+    //For single test case, comment the above 3 lines and uncomment below line
+    solve();
+
+    return 0;
+}
+
+/*
+   Solution Logic:
+  - The sequence is rearranged so that all odd numbers from 1 to n appear first,
+    followed by all even numbers from 1 to n.
+
+  - Total odd numbers in the range [1, n] = (n + 1) / 2.
+
+  - If k is within the range of odd numbers:
+        The k-th position corresponds to the k-th odd number.
+        k-th odd number = 2*k - 1.
+
+  - Otherwise:
+        The element lies in the even part of the sequence.
+        First subtract the number of odd elements:
+            new position = k - (n + 1) / 2
+        new_position-th even number = 2 * new_position.
+
+  - Since n can be as large as 10^12, generating the sequence is impossible,
+    so we compute the result mathematically in O(1) time.
+*/
+
+/*
+  Alhamdulillah, problem solved successfully!
+  الحمد لله الذي بنعمته تتم الصالحات
+*/
